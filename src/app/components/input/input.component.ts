edf53543f2b7fad6ee6f6d69803be8184c056d39
eireneof <you@example.com>
2023-04-18 23:17:06 -0300

    feat: validation and screen update with the provided IP value

--- src/app/components/input/input.component.ts
+++ src/app/components/input/input.component.ts
@@ -1,4 +1,5 @@
-import { Component, Input } from '@angular/core';
+import { Component, ElementRef, EventEmitter, Input, Output, ViewChild } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-input',
@@ -7,6 +8,34 @@ import { Component, Input } from '@angular/core';
 })
 export class InputComponent {
 
+  @ViewChild('inputvalue') inputElementRef!: ElementRef;
+
+  @Output() inputValueEmitter: EventEmitter<string> = new EventEmitter<string>();
+
+  inputValue: any;
+  inputError: boolean = false;
+  ipRegex: RegExp = /^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/;
+
+  handleKeyPressEvent(event: any) {
+  }
+
+  isInputValid(): boolean { 
+    return (this.ipRegex.test(this.inputValue)); 
+  }
+
+  onInputChange(event: any) {
+  }
+
+  submit() {
+    this.inputValue = this.inputElementRef.nativeElement.value;
+
+    if(this.isInputValid()) {
+      this.inputError = false;
+      this.inputValueEmitter.emit(this.inputValue);
+    } else {
+      this.inputError = true;
+    }
+  }
   
 
 }
--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
 import { IpLocationService } from '../services/ip-location-service.service';
+import { IpLocationResponse } from '../models/ip-location-response';
+import { IPAddressInfos } from '../models/ipaddress-infos';
+import { LoadingModalComponent } from '../components/loading-modal/loading-modal.component';
 
 @Component({
   selector: 'app-home-page',
@@ -7,24 +10,55 @@ import { IpLocationService } from '../services/ip-location-service.service';
   styleUrls: ['./home-page.component.scss'],
 })
 export class HomePageComponent {
-  constructor(private _ipLocationService: IpLocationService) {}
 
-  // TODO: fazer um loading
-
-  ngOnInit(): void {
-    this.getIPInformation();
+  @ViewChild('loadingModal', { static: false })
+  loadingModal: LoadingModalComponent;
+  
+  ipResponse: IpLocationResponse = new IpLocationResponse();
+  IPInfos: IPAddressInfos = {
+    ipAdress: '---------------',
+    location: '---------------',
+    timeZone: '---------------',
+    isp: '---------------'
   }
 
-  getIPInformation() {
-    this._ipLocationService.getGeolocationByIp('189.94.22.160').subscribe({
+  constructor(private _ipLocationService: IpLocationService) {}
+
+  ngOnInit(): void {
+
+  }
+
+  // 189.94.22.160
+  searchIPInfos(ip: string) {
+    this.openLoadingModal();
+    this._ipLocationService.getGeolocationByIp(ip).subscribe({
       next: (response: any) => {
         if (response) {
-          console.log(response);
+          this.ipResponse = response;
+          this.populateFieldsIPInfos(response);
+          this.closeLoadingModal();
         }
       },
       error: (error: any) => {
+        // TODO: fazer tratativa de erro
+        this.closeLoadingModal();
         console.log(error);
       },
     });
+  }
+
+  populateFieldsIPInfos(response: IpLocationResponse) {
+    this.IPInfos.location = response.location.country + ' ' + response.location.region;
+    this.IPInfos.timeZone = response.location.timezone;
+    this.IPInfos.isp = response.isp;
+    this.IPInfos.ipAdress = response.ip;
+  }
+
+  openLoadingModal() {
+    this.loadingModal.open();
+  }
+
+  closeLoadingModal() {
+    this.loadingModal.close();
   }
 }
